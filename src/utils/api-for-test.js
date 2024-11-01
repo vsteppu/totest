df58ad71ca09bc2a19c5b741b2bbb8332ed00c9d
Your Name <vlad@wonsulting.com>
2024-11-01 14:44:03 +0200

    something

---
+++ src/utils/api-for-test.js
@@ -0,0 +1,22 @@
+/* const results = [
+    undefined
+] */
+const results = [
+
+    {
+        name:'vlad',
+        id:123
+    },
+    {
+        name:'victor',
+        id: 2345
+    },
+    //undefined,
+    {
+        name:'dana',
+        id: 231
+    }
+]
+export {
+    results
+}
