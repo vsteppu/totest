88c22e6572b5303e3a676fa04d67e7c607ca1814
Your Name <vlad@wonsulting.com>
2024-11-08 13:20:00 +0200

    totest 6

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import { isSocialMediaAppBrowser } from './utils/utils';
 import { onMounted, ref } from 'vue';
 
+const show = ref(false)
+
 const isSocialMediaBrowser = ref(isSocialMediaAppBrowser());
 const ua = navigator.userAgent.toLowerCase();
 
@@ -12,8 +14,8 @@ const detectDevice = () => {
   const isIPhone = ua.includes('iphone')
   const isWindows = ua.includes('windows')
 
-  if (isChrome){
-    
+  if (isChrome) {
+
   }
   if (isIPhone) return 'iPhone';
   if (isAndroid) return 'Android';
@@ -28,25 +30,28 @@ onMounted(() => {
 
 <template>
   <p>
-  Is Social Browser - {{ isSocialMediaBrowser }} <br>
-  Device - {{ detectDevice() }} <br>
-  User Agent - {{ ua }} <br>
+    Is Social Browser - {{ isSocialMediaBrowser }} <br>
+    Device - {{ detectDevice() }} <br>
+    User Agent - {{ ua }} <br>
   </p>
-
   <div>
-<!--     <transition
-    enter-active-class="ease-in-out duration-200"
-    enter-from-class="opacity-0"
-    enter-to-class="opacity-100"
-    
-    leave-active-class="ease-in-out duration-200"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
+    <button @click="show=!show" class=" bg-gray-400 mt-9 p-3 m-4 ">push</button>
+    <transition 
+      enter-active-class="ease-in duration-[0.3s] transform"
+      enter-from-class="-translate-x-full"
+      enter-to-class=" translate-x-0"
       
-      mode="out-in"
+      leave-active-class="ease-out duration-[1s] transform"
+      leave-from-class="translate-x-0"
+      leave-to-class="-translate-x-full"
       >
-      <div class="h-full bg-gray-300">
+      <div v-if="show" class=" flex items-center ml-4 my-4 p-3 h-20 w-96 bg-teal-400">
+        Hello from Rihana
       </div>
-    </transition> -->
-</div>
+      <div v-else class=" flex items-center ml-4 my-4 p-3 h-20 w-96 bg-red-400">
+        Hello from Nikki Minaj
+      </div>
+    </transition>
+
+  </div>
 </template>
