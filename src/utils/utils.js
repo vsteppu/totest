189ec51a4aa07b7b1ac71bf12364e1d406127cd9
Your Name <vlad@wonsulting.com>
2024-11-08 14:35:46 +0200

    totest 8

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,16 +6,16 @@
 export const isSocialMediaAppBrowser = () => {
   const ua = navigator.userAgent.toLowerCase().trim();
   const url = window.location.href;
-  
+
   // Social Media Apps Detection
   const isInstagram = ua.includes('instagram'); //saw on android / saw on IPhone
   const isLinkedIn = ua.includes('linkedin') || ua.includes('li app'); //redirect on android / saw on iphone
   const isFacebook = ua.includes('facebook') || ua.includes('fbav'); //saw on android / saw on IPhone
-  const isThreadsAndroid = ua.includes('threads'); //redirect on android
+  const isThreads = ua.includes('threads') || url.includes('threads.net'); //redirect on android
   const isYouTube = ua.includes('youtube') || ua.includes('crios'); //redirect on android /
   const isTikTok = ua.includes('tiktok') || ua.includes('musically'); // ??? / ???
 
-  /*  
+/*  
       const isChrome = ua.includes('chrome') && !ua.includes('edge') && !ua.includes('opr');
       const isSafari = ua.includes('safari') && !ua.includes('chrome') && !ua.includes('crios');
       if (ua.includes('safari') && !ua.includes('crios') && !ua.includes('chrome')) {
@@ -33,9 +33,10 @@ export const isSocialMediaAppBrowser = () => {
       if (isChrome) return 'Chrome';
       if (isSafari) return 'Safari';
         
-      return 'Unknown Browser'; // Default if no match is found */
+      return 'Unknown Browser'; // Default if no match is found 
+*/
 
-  if (isInstagram || isLinkedIn || isTikTok || isFacebook || isThreadsAndroid || isThreadsthroughIOS || isYouTube || isChrome || isSafari) return true
+  if (isInstagram || isLinkedIn || isTikTok || isFacebook || isThreads || isYouTube ) return true
   return false
 };
 
