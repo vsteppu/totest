bdd0253b147645514f27987bdc41b9d16faf5c8d
Your Name <vlad@wonsulting.com>
2024-11-12 13:26:19 +0200

    002

---
+++ src/utils/storage.js
@@ -0,0 +1,26 @@
+
+const REDIRECT_FROM_SN = [ 
+    'instagram',
+    'barcelona',
+    'linkedin',
+    'li app',
+    'facebook',
+    'fbav',
+    'fban',
+    'fbios',
+    'fbop',
+    'fbdv',
+    'fbmd',
+    'youtube',
+    'YouTube',
+    'crios',
+    'com.google.android.youtube',
+    'youtubeapp',
+    'tiktok',
+    'musically',
+    'bytedancewebview',
+    'musical_ly',
+    'falcontag'
+  ]
+
+  export { REDIRECT_FROM_SN }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,39 +2,9 @@
   const userAgent = navigator.userAgent || navigator.vendor || window.opera;
   return userAgent.indexOf('Instagram') > -1;
 }; */
+import {REDIRECT_FROM_SN} from '../utils/storage'
 
 export const isSocialMediaAppBrowser = () => {
   const ua = navigator.userAgent.toLowerCase().trim();
-
-  // Social Media Apps Detection
-  const REDIRECT_FROM_SN = [ 
-    'instagram',
-    'barcelona',
-    'linkedin',
-    'li app',
-    'facebook',
-    'fbav',
-    'fban',
-    'fbios',
-    'fbop',
-    'fbdv',
-    'fbmd',
-    'youtube',
-    'YouTube',
-    'crios',
-    'com.google.android.youtube',
-    'youtubeapp',
-    'tiktok',
-    'musically',
-    'bytedancewebview',
-    'musical_ly',
-    'falcontag'
-  ]
-
   return REDIRECT_FROM_SN.filter(sn => ua.includes(sn)).length > 0;
-  //return 'Unknown Browser'; // Default if no match is found 
-/* 
-if (isInstagram || isLinkedIn || isTikTok || isFacebook || isThreads || isYouTube  ) return true
-return false
-*/
 };
